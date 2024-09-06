94e1442a5bdd6b527dcfde8738336096ebe229ba
ShadowRagdoll <lilyk5035@gmail.com>
2024-09-06 13:57:19 -0400

    9/6 class

---
+++ 01_gaming_exercises/02_guess_a_number/guessNumber.cs
@@ -0,0 +1,21 @@
+using System;
+class GuessNumber {
+  static void Main() {
+    int numGuess = 0;
+    int maxGuess = 4; 
+
+    // Generate the secret number here.
+    // while loop
+      // Allow the user to guess the number.
+      // Compare the guess to the secret number.
+      // If match, print win, if > or <, give a hint.
+      // +1 to numGuess
+      // Check if numGuess > maxGuesses.
+      // If true, print a "lose game" message, if false guess again.
+      
+
+
+
+
+  }
+}
