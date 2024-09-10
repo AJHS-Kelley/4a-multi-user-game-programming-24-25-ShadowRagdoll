c2bb868b8f5c88b2e9ee99ea11049d49304c0c2e
ShadowRagdoll <lilyk5035@gmail.com>
2024-09-10 14:49:04 -0400

    9/10/24 class

---
+++ 00_computer_science_exercises/03_strings/strings.cs
@@ -0,0 +1,51 @@
+using System;
+class Strings {
+  static void Main() {
+    // In C# ALL STRINGS MUST USE DOUBLE-QUOTES "  "
+
+    string geeting = "Hello, What's your name?\n";
+    string goodbye = "Bye bye.\n";
+
+    // In C# strings are treated like an OBJECT that has properties and methods.
+    // PROPERTIES -- things that describe the object such as length, values, data type, etc.
+    // METHODS -- built in functions related to that object 
+
+    // Length is an important property for strings.
+    Console.WriteLine(geeting.Length);
+    Console.WriteLine(goodbye.Length);
+
+    if (geeting.Length > goodbye.Length)
+    {
+      Console.WriteLine("The greeting has more characters than the goodbye.\n");
+    }
+
+    // Useful String Methods
+
+    Console.WriteLine(greeting.ToUpper()); // Make entire string UPPERCASE
+    Console.WriteLine(greeting.ToLower()); // Make entire string lowercase
+
+    // String Concatenation "Con-Cat-Uh-Nation"
+    string comboString = geeting + goodbye;
+    Console.WriteLine(comboString);
+
+    // String Conactenation Method #2
+    string comboString2 = string.Concat(geeting, goodbye);
+    Console.WriteLine(comboString2);
+
+    // String Interpolation -- Substituting variables into strings.
+    string comboString3 = $"My greeting is {greeting} and my goodbye is {goodbye}.\n";
+    Console.WriteLine(comboString3);
+
+    // Substituting Variables into Strings Method 2
+    Console.WriteLine("My greeting is " + greeting + "and my goodbye is " + goodbye + ".\n");
+
+    // Accessing Parts of Strings
+    // index is a specific location of a character in a string
+    // ALL string indexes start at 0 
+    Console.WriteLine(goodbye[0]); // Print the character at index 0. 
+    Console.WriteLine(goodbye[4]); // Replace x with what to print the 5th char?
+
+    // Where is it in my string?
+    Console.WriteLine(greeting.IndexOf("y")); 
+  }
+}
