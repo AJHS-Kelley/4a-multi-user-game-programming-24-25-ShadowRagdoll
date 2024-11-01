1d0e7fdca2a4c6c5485df2bf81eda3e41945c809
ShadowRagdoll <75490524+ShadowRagdoll@users.noreply.github.com>
2024-11-01 14:01:21 -0400

    11/1 class

--- 00_computer_science_exercises/04_collections/template.cs
+++ 00_computer_science_exercises/04_collections/template.cs
@@ -19,29 +19,46 @@ class Collections {
 
   // DECLARING AND DEFINING AN ARRAY
   string[] breakfastFoods = {"Bacon", "Eggs", "Cold Pizza", "Capn' Crunch", "Gator Tail"};
-  int[] testScore = {0, 25, 15, 75, 99};
+  int[] testScores = {0, 25, 15, 75, 99};
   double[] GPA = {0.27D, 1.99D, 4.02D, 0.0D, 3.25D};
 
   // Print Array Contents -- All Elements on Single Line
-  Console.WriteLine("The elements of each array are:\n");
-  Console.WriteLine("breakFastFoods: \n" + String.Join(", ", breakfastFoods));
-  Console.WriteLine(); // Print an empty line to the screen.
-
+  // Console.WriteLine("The elements of each array are:\n");
+  // Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
+  // Console.WriteLine(); // Print an empty line to the screen.
+  /*
   // Print Array Elements on Separate Lines
   Console.WriteLine("The elements of each array are:\n");
-  Console.WriteLine("testScore: \n" + String.Join("\n", testScore));
+  Console.WriteLine("testScores: \n" + String.Join("\n", testScores));
   Console.WriteLine(); // Print an empty line to the screen.
 
   // How long is my array? DETERMINING ARRAY LENGTH
-  Console.WriteLine("The length of the breakFastFoods array is: " + breakfastFoods.Length);
+  Console.WriteLine("The length of the breakfastFoods array is: " + breakfastFoods.Length);
   // .Length is known as a PROPERTY of the array.
 
   // Accessing Elements in the array with the index.
-  Console.WriteLine("The first element in breakFastFoods is: " + breakfastFoods[0]);
-  Console.WriteLine("The second element in testScores is: " + testScore[1]);
+  Console.WriteLine("The first element in breakfastFoods is: " + breakfastFoods[0]);
+  Console.WriteLine("The second element in testScoress is: " + testScores[1]);
   Console.WriteLine("The third element in GPA is: " + GPA[2]);
+  
+  // Shortcut to Access Last Item in an Array
+  Console.WriteLine("The last element of breakfastFoods is: " + breakfastFoods[breakfastFoods.Length - 1]);
+  Console.WriteLine("The last element of testScores is: " + testScores[testScores.Length - 1]);
 
+  // CHANGING ELEMENTS IN THE ARRAY
+  breakfastFoods[2] = "Food";
+  testScores[0] = -4;
+  GPA[1] = 4.4D;
+  Console.WriteLine("The elements of each array are:\n");
+  Console.WriteLine("breakfastFoods: \n" + String.Join(", ", breakfastFoods));
+  Console.WriteLine(); // Print an empty line to the screen.
+  */
 
+  // COMMON ERRORS WITH ARRAYS
+  // Console.WriteLine(breakfastFoods[99]); // Index Out of Bounds -- Accessing index that does not exist in the array.
+   
+  // testScores[0] = "99"; // Error Message "Cannot implicitly convert dataType to dataType"
+  // Trying to put incorrect data tyoe into the array.
     
   } // DO NOT DELETE EVER, 3 SPACES INDENTED FROM LEFT
 } // DO NOT DELETE EVER, ALWAYS TOUCHING LEFT MARGIN
